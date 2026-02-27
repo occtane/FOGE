884e3a85c997dff429dadcbc40f54222599f2fd2
Cargnx <117575681+Altaiir1@users.noreply.github.com>
2026-02-27 19:36:51 +0100

    [TASK] Fix missing / overload operator

--- main.cpp
+++ main.cpp
@@ -43,6 +43,11 @@ inline Vector3D operator*(const Vector3D &v, float s)
   return (Vector3D(v.x * s, v.y * s, v.z * s));
 }
 
+inline Vector3D operator/(const Vector3D &v, float s)
+{
+  return (Vector3D(v.x / s, v.y / s, v.z / s));
+}
+
 inline Vector3D operator-(const Vector3D &v)
 {
   return (Vector3D(-v.x, -v.y, -v.z));
