8cea750b69af8243d4fd5ff83151f30df63b065d
Cargnx <117575681+Altaiir1@users.noreply.github.com>
2026-02-27 20:00:07 +0100

    [WIP] Separate vector3D and inline function into own vec3 header

--- main.cpp
+++ main.cpp
@@ -1,67 +1,7 @@
 #include <cmath>
 #include <iostream>
 
-struct Vector3D
-{
-  float x{}, y{}, z{};
-
-  Vector3D() = default;
-  Vector3D(float a, float b, float c) : x(a), y(b), z(c)
-  {
-  }
-
-  float &operator[](int i)
-  {
-    return ((&x)[i]);
-  }
-
-  const float &operator[](int i) const
-  {
-    return ((&x)[i]);
-  }
-
-  Vector3D &operator*=(float s)
-  {
-    x *= s;
-    y *= s;
-    z *= s;
-    return (*this);
-  }
-
-  Vector3D &operator/=(float s)
-  {
-    s = 1.0F / s;
-    x *= s;
-    y *= s;
-    z *= s;
-    return (*this);
-  }
-};
-
-inline Vector3D operator*(const Vector3D &v, float s)
-{
-  return (Vector3D(v.x * s, v.y * s, v.z * s));
-}
-
-inline Vector3D operator/(const Vector3D &v, float s)
-{
-  return (Vector3D(v.x / s, v.y / s, v.z / s));
-}
-
-inline Vector3D operator-(const Vector3D &v)
-{
-  return (Vector3D(-v.x, -v.y, -v.z));
-}
-
-inline float Magnitude(const Vector3D &v)
-{
-  return (std::sqrt(v.x * v.x + v.y * v.y + v.z * v.z));
-}
-
-inline Vector3D Normalize(const Vector3D &v)
-{
-  return (v / Magnitude(v));
-}
+#include "include/vec3.h"
 
 int main()
 {
@@ -95,6 +35,4 @@ int main()
   Vector3D unit = Normalize(v);
   std::cout << "|v| = " << len << "\n";
   std::cout << "Normalize(v) = (" << unit.x << ", " << unit.y << ", " << unit.z << ")\n";
-
-  return 0;
 }
